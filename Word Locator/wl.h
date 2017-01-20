05cf1c2451597c4e21b6b9fa152207b6b989ca75
Kirthanaa Raghuraman <kirthan18@gmail.com>
2017-01-20 11:07:43 -0600

    Added comments

--- Word Locator/wl.h
+++ Word Locator/wl.h
@@ -12,6 +12,10 @@
 #include <vector>
 #include <string>
 
+/**
+ * Parses the command to remove any extraneous spaces and also converts all strings to lower case.
+ * @return A vector of strings in lowercase.
+ */
 std::vector<std::string> ParseCommand(std::string);
 
 #endif
